f3102c652eb86ff2a3b5c946255151694550e7e8
Justin M Smith <j.m.smith@utexas.edu>
2014-09-19 12:37:44 -0500

    updated push

--- psh.c
+++ psh.c
@@ -105,6 +105,48 @@ int main(int argc, char **argv)
 */
 void eval(char *cmdline) 
 {
+    char *argv[MAXARGS];        /* Argument list execve() */
+    char buf[MAXLINE];          /* Holds modified command line */
+    int bg;                     /* Should the job run in bg or fg? */
+    pid_t pid;                  /* Process id */
+    
+    strcpy(buf, cmdline);
+    bg = parseline(buf, argv);
+    if (argv[0] == NULL) return;   /* Ignore empty lines */
+    
+    if (!builtin_command(argv))
+    {if ((pid = Fork()) == 0)       /* Child runs user job */
+     {if (execve(argv[0], argv, environ) < 0)
+      {     printf("%s: Command not found.\n", argv[0]);
+            exit(0);
+      }
+     }
+     /* Parent waits for foreground job to terminate */
+     if (!bg)
+     {
+         int status;
+            if (waitpid(pid, &status, 0) < 0)
+                unix_error("waitfg: waitpid error");
+     } else printf("%d %s", pid, cmdline);
+    }
+    return;
+}
+
+
+/* If first arg is a builtin command, run it and return true */
+int builtin_command(char **argv)
+{
+    if (!strcmp(argv[0], "quit")) /* quit command */
+        exit(0);
+    if (!strcmp(argv[0], "&"))
+        return 1;
+    return 0;
+}
+    /* Ignore singleton & */
+    /* Not a builtin command */
+    
+    
+/*
 		int status;
 		//initialize array of arguments
   	char** args = (char**)malloc(20*sizeof(char*));
@@ -126,7 +168,8 @@ void eval(char *cmdline)
 				wait(&status);}
 			}
     return;
-}
+ */
+
 
 
 /* 
